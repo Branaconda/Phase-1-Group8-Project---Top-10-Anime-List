f90053f80a5fc8a2ba82ba6beced954874de9d43
Sarah Baughman <sarahbaughman92@gmail.com>
2023-04-05 13:06:45 -0400

    started index.js

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
+// document.addEventListener('DOMContentLoaded', () => fetchAnime)
+
+const mainUrl = "http://localhost:3000/"
+const animeUrl = mainUrl + "anime/"
+
+
+function fetchAnime(){
+    fetch(animeUrl)
+    .then(response => response.json())
+    .then(animeData => iterateFunction(animeData))
+}
+
+fetchAnime()
+
+function iterateFunction(animeData){
+    animeData.forEach(anime => console.log(anime))
+}
+
+function renderAnime(anime) {
     
+}
