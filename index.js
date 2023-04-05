0ccdc305258cdaf78d2c1e67bd0695aac2b876e5
Sarah Baughman <sarahbaughman92@gmail.com>
2023-04-05 16:14:21 -0400

    stupid like button wiggle

--- index.js
+++ index.js
@@ -14,9 +14,50 @@ function fetchAnime(){
 fetchAnime()
 
 function iterateFunction(animeData){
-    animeData.forEach(anime => console.log(anime))
+    animeData.forEach(anime => renderAnime(anime))
 }
 
 function renderAnime(anime) {
     
+    const animeName = document.createElement('p')
+    animeName.innerText = anime.name
+    
+    const animeImage = document.createElement('img')
+    animeImage.src = anime.image;
+    animeImage.alt = anime.alt;
+
+    if (anime.id <= 5){
+        const leftImages = document.getElementById("left-images")
+        animeImage.className = "left"
+        leftImages.appendChild(animeImage)
+        leftImages.appendChild(animeName)
+        
+        
+    } else {
+        const rightImages = document.getElementById("right-images")
+        animeImage.className = "right"
+        rightImages.appendChild(animeImage)
+        rightImages.appendChild(animeName)
+    }
+    animeImage.addEventListener('click', () => {
+        const bigPicture = document.getElementById("default-anime-image")
+        bigPicture.src = anime.image
+
+        const bigTitle = document.getElementById("anime-title")
+        bigTitle.innerText = anime.name
+
+        const bigSummary = document.getElementById("anime-description-body")
+        bigSummary.innerText = anime.description
+    })
 }
+
+function likeButton() {
+    const likeButton = document.getElementById("buttons")
+    likeButton.addEventListener('mouseover', () => {
+        document.getElementById("like-button").hidden = true;
+        document.getElementById("like-button2").hidden = false
+    },
+    false
+)}
+
+likeButton()
